312e6b9dbca8fa2a6c50c8bacc042f4218267c35
Ricardo Paiva <ricardoccpaiva@gmail.com>
2015-11-13 08:49:45 +0000

    added more code conventions samples

---
+++ CodeConventionsSample.Managers/MyManager.cs
@@ -0,0 +1,34 @@
+﻿using CodeConventionsSample.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeConventionsSample.Managers
+{
+    public class SampleManager : ISampleManager
+    {
+        private ISampleRepository repository;
+
+        public SampleManager(ISampleRepository repo)
+        {
+            repository = repo;
+        }
+
+        public int DoSomeCalculations(List<string> lst)
+        {
+            var ret = 0;
+
+            foreach (var item in lst)
+            {
+                if (item.Length % 2 == 0)
+                {
+                    ret++;
+                }
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ CodeConventionsSample.WebApi/Controllers/SampleController.cs
@@ -0,0 +1,38 @@
+﻿using CodeConventionsSample.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CodeConventionsSample.WebApi.Controllers
+{
+    [RoutePrefix("api/Sample")]
+    public class SampleController : ApiController
+    {
+        private const string LocalLoginProvider = "Local";
+        private ISampleManager _Manager;
+
+        public SampleController(ISampleManager SampleManager)
+        {
+            _Manager = SampleManager;
+        }
+
+        [Route("Test")]
+        public int Calculate()
+        {
+            var param = new List<string>() { "val1", "va2", "batata", "cacau", "cenas_malucas" };
+
+            return _Manager.DoSomeCalculations(param);
+        }
+
+        [Route("Magic")]
+        public string Validate(string HTMLTag)
+        {
+            if(string.IsNullOrEmpty(HTMLTag)) { return "invalid"; }
+
+            return null;
+        }
+    }
+}
